711899259a5a5d34c15c70904b7de7574e335007
Zavan7 <vitorzavanj@gmail.com>
2024-12-09 12:52:14 -0300

    Dia 32

--- openpyxl/29-aula1_openpyxl.py
+++ openpyxl/29-aula1_openpyxl.py
@@ -7,8 +7,18 @@ import openpyxl
 PASTA_RAIZ = Path(__file__).parent
 WORKBOOK_PATH = PASTA_RAIZ / 'workbook.xlsx'
 
-workbook = openpyxl.Workbook()
-sheet = 
+workbook = Workbook()
+
+name_sheet = 'Minha Planilha'
+workbook.create_sheet(name_sheet, 0)
+sheet = Worksheet = workbook[name_sheet]
+
+#Remover uma planila
+workbook.remove(workbook['Sheet'])
+
+sheet.cell(1, 1, 'Nome')
+sheet.cell(1, 2, 'Idade')
+sheet.cell(1, 3, 'Nota')
 
 students = [
     # nome      idade nota
@@ -17,3 +27,13 @@ students = [
     ['Luiz',    15,   8.8],
     ['Alberto', 16,   10],
 ]
+
+#TEM ESSA OPCAO TAMBÉM
+# for student in students:
+#     sheet.append(student)
+
+for i, students_row in enumerate(students, start=2):
+    for j, studants_column in enumerate(students_row, start=1):
+        sheet.cell(i, j, studants_column)
+
+workbook.save(WORKBOOK_PATH)
